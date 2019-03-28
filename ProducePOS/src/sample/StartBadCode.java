c2f95f88271e3fd459c255af1d81de2ff63c7716
Nick <tnabguy@gmail.com>
2019-03-28 19:01:30 -0400

    lol

---
+++ ProducePOS/src/sample/StartBadCode.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StartBadCode {
+    private static Stage primaryStage = new Stage();
+    public void start(){
+
+    }
+
+    public static void killProcess(){
+        primaryStage.close();
+    }
+}

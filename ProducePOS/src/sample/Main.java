546040f91e6af2226eb8c0ae3316ed0873b18c1d
Nick <tnabguy@gmail.com>
2019-03-29 12:33:44 -0400

    Stable release

--- ProducePOS/src/sample/Main.java
+++ ProducePOS/src/sample/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
